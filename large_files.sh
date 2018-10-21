b9044fbcb1747e07a1a49e6fe92701f53c1379c3
Igor Os <igoros777@gmail.com>
2018-10-21 08:49:33 -0400

    Update large_files.sh

--- large_files.sh
+++ large_files.sh
@@ -4,5 +4,5 @@ l="${2}"; if [ -z "${l}" ]; then l=10; fi
 h=$(echo ${HOSTNAME} | awk -F. '{print $1}')
 d=$(date +'%Y-%m-%d %H:%M:%S')
 for i in ${f}; do
-  find "${i}" -xdev -printf "${d},${h},%s,%TY-%Tm-%Td_%TH:%TM,%u:%g,%m,%n,%p\n" | sort -rn | head -${l}
-done | sort -t, -k3rn | (echo "DATE,HOST,KB,MTIME,UID:GID,RWX,HL,PATH" && cat) | column -s',' -t
+  find "${i}" -xdev -printf "${d},${h},%s,%TY-%Tm-%Td_%TH:%TM,%u:%g,%m,%n,%p\n" | sort -t, -k3rn | head -${l}
+done | (echo "DATE,HOST,KB,MTIME,UID:GID,RWX,HL,PATH" && cat) | column -s',' -t
