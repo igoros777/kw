65bba66833d8ebf8e560cce514b1b27d25489a10
Igor Os <ichmod444@gmail.com>
2020-01-26 14:30:16 -0500

    Create code2png.sh

---
+++ code2png.sh
@@ -0,0 +1,20 @@
+  #!/bin/bash
+  fmt=svg
+  out=png
+  ext="${3}"
+  wsize=1280
+  source_path="${1}"
+  target_path="${2}"
+  if [ -z "${source_path}" ] || [ -z "${target_path}" ]; then exit 1; fi
+  if [ ! -f "${source_path}" ] && [ -z "${ext}" ]; then exit 3; fi
+  if [ -f "${source_path}" ] && [ -z "${ext}" ]; then ext="*"; fi
+  if [ ! -d "${target_path}" ]; then mkdir -p "${target_path}" || exit 2; fi
+  find "${source_path}" -maxdepth 1 -mindepth 0 -type f -name "*\.${ext}" | while read f
+  do
+    pygmentize -O "style=monokai,fontface=DejaVu Sans Mono,fontsize=24" -f "${fmt}" \
+    -l bash -o "${target_path}/$(basename -- "${f%.*}").${fmt}" "${f}" 2>/dev/null
+    inkscape -z -D --export-area-snap -w ${wsize} \
+    "${target_path}/$(basename -- "${f%.*}").${fmt}" \
+    -e "${target_path}/$(basename -- "${f%.*}").${out}" 2>/dev/null
+    /bin/rm "${target_path}/$(basename -- "${f%.*}").${fmt}" 2>/dev/null
+  done
