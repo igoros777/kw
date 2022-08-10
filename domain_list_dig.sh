f8931f4013261a351ecb752223715936cba22f68
Igor Os <igoros777@gmail.com>
2022-08-10 15:15:46 -0400

    Update domain_list_dig.sh

--- domain_list_dig.sh
+++ domain_list_dig.sh
@@ -2,23 +2,24 @@
 list="${1}"
 if [ ! -r "${list}" ]; then exit 1 ; fi
 cat "${list}" | \
-while read i; do
-levels=$(echo "${i}" | sed 's/[^.]//g' | awk '{ print length+1 }')
-array_variables=($(for j in $(seq 1 ${levels}); do echo s${j}ld; done))
-array_values=($(for l in $(seq ${levels} -1 1); do echo "${i}" | awk -F. -v f=${l} '{s = ""; for (i = f; i <= NF; i++) s = s$i " "; print s }' | sed 's/\s/./g' | sed 's/\.$//g'; done))
-for ((k = 0; k < ${#array_variables[@]}; k++))
+while read i
 do
-  if [ -z "$(eval echo $(echo $`eval echo "${array_variables[$k]}"`))" ]
-  then
+  unset array_variables array_values
+  levels=$(echo "${i}" | sed 's/[^.]//g' | awk '{ print length+1 }')
+  array_variables=($(for j in $(seq 1 ${levels}); do echo s${j}ld; done))
+  array_values=($(for l in $(seq ${levels} -1 1); do echo "${i}" | awk -F. -v f=${l} '{s = ""; for (i = f; i <= NF; i++) s = s$i " "; print s }' | sed 's/\s/./g' | sed 's/\.$//g'; done))
+  for ((k = 0; k < ${#array_variables[@]}; k++))
+  do
     eval "$(echo "${array_variables[$k]}")"="$(echo "\"${array_values[$k]}\"")"
-  fi
-done
-dig ${i} @$(dig ns ${i} | \
-egrep -m1 "^${i}.*IN.(CNAME|NS)" | awk '{print $NF}' | \
-sed 's/\.$//g') -t any | grep -v \; | grep . | while read line; do
-t=$(echo "${line}" | awk '{print $4}')
-r=$(echo "${line}" | awk '{print $1}')
-ttl=$(echo "${line}" | awk '{print $2}')
-v=$(echo "${line}" | awk '{print $NF}' | sed 's/\.$//g')
-echo "${i:-none},${s1ld:-none},${s2ld:-none},${s3ld:-none},${s4ld:-none},${t:-none},${ttl:-none},${v:-none}"
-done; done | (echo "Record,TLD,SLD,3LD,4LD,Type,TTL,Value" && cat)
+  done
+  dig ${i} @$(dig ns ${i} | \
+  egrep -m1 "^${i}.*IN.(CNAME|NS)" | awk '{print $NF}' | \
+  sed 's/\.$//g') -t any | grep -v \; | grep . | while read line
+  do
+    t=$(echo "${line}" | awk '{print $4}')
+    r=$(echo "${line}" | awk '{print $1}')
+    ttl=$(echo "${line}" | awk '{print $2}')
+    v=$(echo "${line}" | awk '{print $NF}' | sed 's/\.$//g')
+    echo "${i:-none},${s1ld:-none},${s2ld:-none},${s3ld:-none},${s4ld:-none},${t:-none},${ttl:-none},${v:-none}"
+  done
+done | (echo "Record,TLD,SLD,3LD,4LD,Type,TTL,Value" && cat)
